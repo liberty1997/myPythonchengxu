b77a6ad4f6b7f79616a0caeea10a30552d3b0c6e
liberty1997 <liberty1997@yeah.net>
2017-04-28 00:10:39 +0800

    whatever

--- python_practices/er_fen_qiu_zhi/copy_er_fen_qiu_zhi.py
+++ python_practices/er_fen_qiu_zhi/copy_er_fen_qiu_zhi.py
@@ -4,8 +4,8 @@
 import math
 
 def fangchengshi(x) :
-    F = x**3 + 4*(x**2) -10
-    #F = math.exp(x) + 10 * x - 2
+    #F = x**3 + 4*(x**2) -10
+    F = math.exp(x) + 10 * x - 2
     return F
 
 def func(low,high) :
@@ -20,7 +20,7 @@ def func(low,high) :
     print("h = %.8f" % h)
     print("m = %.8f" % m)
     #if round(mm,8) - mm != 0.0 :
-    if (h-l)/2.0 < (0.5*0.001) :
+    if (h-l)/2.0 < (0.5*(10**(-8))) :
         print("Ans : %.8f" % mm)
         return
     elif l * m < 0 :
@@ -29,4 +29,5 @@ def func(low,high) :
         func(mm,high)
 
 if __name__ == '__main__' :
-    func(1.0,2.0)
+    #func(1.0,2.0)
+    func(0.0,1.0)
