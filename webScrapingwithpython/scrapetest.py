f455000d90a76a480b713323782985af9cfbac00
liberty1997 <liberty1997@yeah.net>
2017-05-01 17:34:22 +0800

    webscrapingwithpython

---
+++ webScrapingwithpython/scrapetest.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+def getTitle(url) :
+    try :
+        html = urlopen(url)
+    except HTTPError as e :
+        return None
+    try :
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+        #title = bsObj.h1
+    except AttributeError as e :
+        return None
+    return title
+title1 = getTitle("http://www.pythonscraping.com/pages/page1.html")
+title2 = getTitle("http://www2015.tyut.edu.cn/cn/index.html")
+for t in title1,title2 :
+    if t == None :
+        print("Title could not be found")
+    else :
+        print(t)
