248bc9797c0a721ef06c16c1466f308b3c85d91b
liberty1997 <liberty1997@yeah.net>
2017-05-05 20:20:49 +0800

    An evil Monster.Bad Day 2017/05/05

--- python_practices/er_fen_qiu_zhi/er_fen_qiu_zhi.py
+++ python_practices/er_fen_qiu_zhi/er_fen_qiu_zhi.py
@@ -1,31 +1,29 @@
-#!/bin/python
+#!/bin/python3
 # -*- coding: UTF-8 -*-
 
 #import math
 
 def fangchengshi(x):
     F = x**3 + 4*(x**2) - 10
-    #F = x**2 - 2*x + 1
-    #F = math.exp(x)+10*x-2
     return F
 
 def func(low,high,n):
     n = n + 1
     l = fangchengshi(low)
     h = fangchengshi(high)
-    mm = (low + high ) * 0.5
+    mm = (low + high ) / 2
     m = fangchengshi(mm)
-    #print("mm = %.8f" % mm)
+    print("mm= ", mm)
     # 符合精度,到达递归出口
-    if (m ==0 ) or (((h-l) * 0.5) < (0.5 * 10**(-5))):
-        print("Ans : ",mm)
-        print("次数: %d" % n)
+    if m == 0 or low==high or (high-low) / 2 < (0.5 * 10**(-5)):
+        print("Ans :%.5f" % mm)
+        print("次数:", n)
         return
     # 左半
     if l * m < 0:
         func(low,mm,n)
     # 右半
-    elif m * h < 0:
+    if m * h < 0:
         func(mm,high,n)
 
 if __name__ == '__main__':
--- python_practices/newTonfa/newTonfa.py
+++ python_practices/newTonfa/newTonfa.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 # -*- coding: UTF-8 -*-
 
-
 def fangcheng(x):
     F = x**3 + 4*(x**2) -10
     return F
@@ -12,16 +11,16 @@ def yidao(x):
 
 def Newton(x,n):
     n = n + 1
+    print("x1= " ,x)
     x1 = x - fangcheng(x) / yidao(x)
     # 符合精度,到达递归出口
     if abs(x1 - x) < 0.5 * 10**(-5):
-        print("Ans: ",x)
-        print("次数: %d" % (n-1))
+        print("Ans: %.5f" % x)
+        print("次数: %d" % n)
         return
     else:
         # 继续迭代
         Newton(x1,n)
-
 
 if __name__ == '__main__':
     Newton(1.5,0)
