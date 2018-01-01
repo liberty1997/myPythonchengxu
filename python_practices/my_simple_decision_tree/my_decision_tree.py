39491e3fdc1cc230934977ba0e5884d9925e69d0
liberty1997 <liberty1997@yeah.net>
2018-01-01 09:48:48 +0800

    my_simple_decision_tree_v002

--- python_practices/my_simple_decision_tree/my_decision_tree.py
+++ python_practices/my_simple_decision_tree/my_decision_tree.py
@@ -11,11 +11,20 @@ def Load_data(fname):
 			data.append(line.replace('\n', '').split(' ')[1:])
 	return data
 
-def Generate_decision_tree(D, attribute_list):
+def Load_test(fname):
+	test = []
+	with open(fname) as f:
+		for line in f.readlines():
+			test.append(line.replace('\n', '').split(' '))
+	return test
+
+def Generate_decision_tree(D, attribute_list, tree, k):
 	'''
 	mark == 0 返回的是类
 	mark == 1 返回的是属性
 	'''
+	print('D===', D)
+	print('path==', tree)
 	D_class = {}
 	for i in range(len(D)):
 		if D[i][-1] not in D_class.keys():
@@ -24,28 +33,20 @@ def Generate_decision_tree(D, attribute_list):
 	
 	# D中元组同属一类
 	if len(D_class) == 1:
-		print('Generate Return==0', list(D_class)[0])
-		return 0, list(D_class)[0]
+		class_same = list(D_class)[0]
+		tree.append([class_same])
+		return 0, class_same
 	
 	# attribute_list为空时, 返回多数类
 	if len(attribute_list) == 0:
 		D_class = sorted(D_class.items(), key = lambda a:a[1], reverse = True)
-		print('Generate Return==0 attrlist empty', D_class[0][0])
-		return 0, D_class[0][0]
+		class_most = D_class[0][0]
+		tree.append([class_most])
+		return 0, class_most
 	
 	# 属性度量选择, 信息增益
 	# 返回的N代表某个属性, 如 0 1 2 3 4 ... ...
 	N = Attribute_selection_method(D, attribute_list)
-	'''
-	# 从attribute_list和D中删除属性N
-	for i in range(len(attribute_list)):
-		if attribute_list[i] == N:
-			for j in range(len(D)):
-				del D[j][i]
-			#del attribute_list[i]
-			#break
-	del attribute_list[-1]
-	'''	
 	N_value = set()
 	for i in range(len(D)):
 		N_value.add(D[i][N])
@@ -56,34 +57,49 @@ def Generate_decision_tree(D, attribute_list):
 		D1 = copy.deepcopy(D)
 		attr_list = copy.deepcopy(attribute_list)
 		for i in range(len(D1)):
-			#print('i=',i,' N=', N, 'nv=',nv)
 			if D1[i][N] == N_value[nv]:
 				D_split.append(D1[i])
 		# 从attribute_list和D中删除属性N
 		for i in range(len(attr_list)):
 			if attr_list[i] == N:
-				print('attr=', i + 1)
+				'''
+				if k != 0:
+					print('attr=', N + 1)
+				else:
+					print('attr=', N)
+				'''
 				for j in range(len(D1)):
 					del D1[j][i]
 				break
+		#print('attr_list BERORE', attr_list)
 		del attr_list[-1]
+		#print('attr_list AFTER', attr_list)
 		if len(D_split) == 0:
-			print('Generate Return ==0 nv==', N_value[nv], D_class[0][0])
-			return 0, D_class[0][0]
+			class_empty = [0, D_class[0][0]]
+			tree.append(class_empty)
+			return 0, class_empty
 		else:
-			print('nv=', N_value[nv])
-			Generate_decision_tree(D_split, attr_list)
-			print('BACK-------')
-	
-	print('Generate Return==1', N)
+			if k != 0:
+				class_ = [N + 1, N_value[nv]]
+				if class_ not in tree:
+					tree.append(class_)
+			else:
+				class_ = [N, N_value[nv]]
+				if class_ not in tree:
+					tree.append(class_)
+			Generate_decision_tree(D_split, attr_list, tree, k + 1)
 	return 1, N
 
 def Attribute_selection_method(D, attribute_list):
 	# 计算哪个属性的Info最小, 即为其信息增益Gain最大
 	N = 'unknown' # 返回的具有最大信息增益的属性
 	min_Info = 99999
-	lenD = len(D)
 	print('attribute_list', attribute_list)
+	lenD = len(D)
+	'''
+	if len(attribute_list) == 1:
+		return attribute_list[0]
+	'''
 	for i in range(len(attribute_list)):
 		Info_v = 0.0
 		attr_value = {} # 属性的值的次数
@@ -103,16 +119,51 @@ def Attribute_selection_method(D, attribute_list):
 				p = q[1] / k[1]
 				temp += ((-p) * math.log(p, 2))
 			Info_v += ((k[1] / lenD) * temp)
-		if Info_v < min_Info:
+		print('Info_v==', Info_v)
+		if Info_v < min_Info and Info_v != min_Info:
 			min_Info = Info_v
 			N = attribute_list[i]
-	#print('Attribute_selection_method:', min_Info)
 	return N
-				
+
+def Predict(test, tree):
+	for i in range(len(test)):
+		pair = []
+		for j in range(len(test[i])):
+			pair.append([j, test[i][j]])
+		print(pair)
+		start = tree[0][0]
+		for k in range(len(tree)):
+			if tree[k][0] == start and tree[k][1] == pair[start][1]:
+				start = k
+				break
+		while True:
+			print('tree[start][0]=', tree[start][0])
+			print('start=', start)
+			if pair[tree[start][0]][1] == tree[start][1]:
+				if len(tree[start + 1]) == 1:
+					print(tree[start + 1][0])
+					break
+				start += 1
+			else:
+				start += 2
+
+		
+
 
 if __name__ == '__main__':
-	#D = Load_data('lenses.data')
-	D = Load_data('AllElectronics.csv')
-	tree = {}
+	D = Load_data('lenses.data.train')
+	T = Load_test('my_decision_tree_test.csv')
+	#D = Load_data('AllElectronics2.csv')
+	#T = Load_test('AllElectronics_test.csv')
+	#print('D===', D)
+	#print('T===', T)
+	tree = []
+	# k用来标记深度. 每深入一层, 每依据一个属性划分一次,
+	# attribute_list就要删去那个属性, 所以除了第一次, 其后每次输出N时都应该加1
+	# k的作用: k是根据个人需要添加的!
+	k = 0
 	attribute_list = [ _ for _ in range(len(D[0]) - 1) ]
-	Generate_decision_tree(D, attribute_list)
+	Generate_decision_tree(D, attribute_list, tree, k)
+	print(tree)
+	Predict(T, tree)
+
